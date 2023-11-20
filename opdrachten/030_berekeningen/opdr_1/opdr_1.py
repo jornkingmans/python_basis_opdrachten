35d144c1b88cf655c3690918a6312fcb4fcc9e2b
jornkingmans <151020968+jornkingmans@users.noreply.github.com>
2023-11-20 21:08:58 +0100

    Update opdr_1.py

--- opdrachten/030_berekeningen/opdr_1/opdr_1.py
+++ opdrachten/030_berekeningen/opdr_1/opdr_1.py
@@ -1,5 +1,15 @@
 # Opdracht 1 berekeningen
-# Naam student:
+# Naam student: Jorn Kingmans
 # Groep:
 
 # Hier komt je code...
+Joris = 21
+Daan = 21
+Arno = 22
+
+totale_leeftijd = Joris + Daan + Arno
+
+gemiddelde_leeftijd = totale_leeftijd / 3
+
+print("De totale waarde van alle leeftijden bij elkaar opgeteld =", totale_leeftijd)
+print("De gemiddelde leeftijd van deze boys is:", gemiddelde_leeftijd)
