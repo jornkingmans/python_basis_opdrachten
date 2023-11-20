6fa32b451518bae955f85bc7e36b661e1e160eb9
jornkingmans <151020968+jornkingmans@users.noreply.github.com>
2023-11-20 20:18:29 +0100

    Update opdr_2.py

--- opdrachten/020_tekstfuncties/opdr_2/opdr_2.py
+++ opdrachten/020_tekstfuncties/opdr_2/opdr_2.py
@@ -1,9 +1,10 @@
 # Opdracht 1
-# Naam student:
+# Naam student: Jorn Kingmans
 # Groep:
 
 # Hier komt je code...
 
-my_list = ...
+my_string = "Tinus gaat op zijn tandem naar de hottentottententoonstelling"  
 
-print()
+x = my_string.count("t")
+print(x)
