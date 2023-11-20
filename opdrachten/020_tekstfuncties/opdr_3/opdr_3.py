d715b215a09d368817cdec192b197fb84a41efb0
jornkingmans <151020968+jornkingmans@users.noreply.github.com>
2023-11-20 20:33:41 +0100

    Update opdr_3.py

--- opdrachten/020_tekstfuncties/opdr_3/opdr_3.py
+++ opdrachten/020_tekstfuncties/opdr_3/opdr_3.py
@@ -1,5 +1,22 @@
 # Opdracht 3 tekstfuncties
-# Naam student:
+# Naam student: Jorn Kingmans
 # Groep:
 
 # Hier komt je code...
+boom_top = "     *    " * 5
+boom_midden_1 = "    ***   " * 5
+boom_midden_2 = "   *****  " * 5
+boom_midden_3 = "  ******* " * 5
+boom_bodem = " *********" * 5
+stam = "    ***   " * 5
+
+print(boom_top)
+print(boom_midden_1)
+print(boom_midden_2)
+print(boom_midden_3)
+print(boom_bodem)
+print(stam)
+print(stam)
+print(stam)
+
+# na veel kloten kom je ergens :)
