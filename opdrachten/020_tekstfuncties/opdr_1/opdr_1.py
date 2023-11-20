cd6d66f9cc0e8d0585503f8c3eba8499e31da4d2
jornkingmans <151020968+jornkingmans@users.noreply.github.com>
2023-11-20 20:13:19 +0100

    Update opdr_1.py

--- opdrachten/020_tekstfuncties/opdr_1/opdr_1.py
+++ opdrachten/020_tekstfuncties/opdr_1/opdr_1.py
@@ -1,5 +1,5 @@
 # Opdracht 1
-# Naam student:
+# Naam student: Jorn Kingmans
 # Groep:
 
 # Hier komt je code...
@@ -7,13 +7,14 @@
 # Opdracht 1a
 # Zorg dat de volgende zin op het scherm wordt getoond met de print-functie:
 # O mamma mia, wat maak je toch heerlijke pizza's
-
-print()
+list= "O mamma mia, wat maak je toch heerlijke pizza's"
+print(list)
 
 # Gebruik onderstaande variabelen en de print-functie
 # en toon de volgende zin op het scherm: Albert Einstein zei ooit: 'Als je niks fout doet, dan kun je ook niks leren!'
 voornaam = "albert"
 achternaam = "einstein"
 
-
-print()
+voornaam = "Albert"
+achternaam = "Einstein"
+print(voornaam + " " + achternaam + ' zei ooit: "Als je niks fout doet, dan kun je ook niks leren!"')
