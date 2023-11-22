09f8e1cc5533fa5cdbbc02feee52bee807f651d5
jornkingmans <151020968+jornkingmans@users.noreply.github.com>
2023-11-22 20:31:21 +0100

    Update opdr_2.py

--- opdrachten/040_lists/opdr_2/opdr_2.py
+++ opdrachten/040_lists/opdr_2/opdr_2.py
@@ -1,5 +1,5 @@
 # Opdracht 2 lists
-# Naam student:
+# Naam student: Jorn Kingmans
 # Groep:
 
 
@@ -13,3 +13,14 @@ rivieren = list(rivier_info.keys())
 # rivieren is nu een list met alleen de riviernamen: ['rijn', 'maas', 'nijl']
 
 # Hier jouw code.....
+
+# Print de naam van de 1e rivier en het 2e land waar deze doorheen stroomt
+print("De rivier", rivieren[0].title(), "stroomt onder andere door", rivier_info[rivieren[0]][1].title())
+
+# Print de naam van de 2e rivier en het 1e land waar deze doorheen stroomt
+print("De rivier", rivieren[1].title(), "stroomt onder andere door", rivier_info[rivieren[1]][0].title())
+
+# Print de naam van de 3e rivier en het 3e land waar deze doorheen stroomt
+print("De rivier", rivieren[2].title(), "stroomt onder andere door", rivier_info[rivieren[2]][2].title())
+
+#hoofdletters zijn blijkbaar ook mogelijk met .capitalize zie ik op internet? Hoe zit dit precies?
