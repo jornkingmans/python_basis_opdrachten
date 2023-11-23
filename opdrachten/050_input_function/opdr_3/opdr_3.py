f5a2491429c1074dc35d5f4933672467d463fdf8
jornkingmans <151020968+jornkingmans@users.noreply.github.com>
2023-11-23 11:49:43 +0100

    Update opdr_3.py

--- opdrachten/050_input_function/opdr_3/opdr_3.py
+++ opdrachten/050_input_function/opdr_3/opdr_3.py
@@ -1,6 +1,15 @@
 # Opdracht 3 input functie
-# Naam student:
+# Naam student: Jorn Kingmans
 # Groep:
 
 # Hier komt je code...
 
+steden = []
+
+for i in range(5):
+    stad = input(f"Voer stad nummer {i+1} in: ")
+    steden.append(stad)
+
+steden.sort(reverse=True)
+
+print(steden)
