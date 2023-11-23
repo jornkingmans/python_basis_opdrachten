82fdd966aef2da5815811c352acf9ce4d80070fb
jornkingmans <151020968+jornkingmans@users.noreply.github.com>
2023-11-23 12:24:55 +0100

    Update opdr_3.py

--- opdrachten/060_loops/opdr_3/opdr_3.py
+++ opdrachten/060_loops/opdr_3/opdr_3.py
@@ -1,5 +1,5 @@
 # Opdracht 3 input functie
-# Naam student:
+# Naam student: Jorn Kingmans
 # Groep:
 
 # Hier komt je code...
@@ -7,3 +7,11 @@
 # Hier start de for-loop
 
 my_list = []
+
+for i in range(3, 82, 3):
+    resultaat = (i**2) / 3
+    if resultaat > 2028.2:
+        break
+    my_list.append(resultaat)
+
+print (my_list)
