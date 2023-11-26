12dd0b34da4cf35de0d3072ec58ff59092b01953
jornkingmans <151020968+jornkingmans@users.noreply.github.com>
2023-11-26 21:12:47 +0100

    Update opdr_6.py

--- opdrachten/060_loops/opdr_6/opdr_6.py
+++ opdrachten/060_loops/opdr_6/opdr_6.py
@@ -1,9 +1,25 @@
 # Opdracht 3 input functie
-# Naam student:
+# Naam student: Jorn Kingmans
 # Groep:
 
 # Hier komt je code...
 
 # Hier start de for-loop
 
-my_list = []
+pizzas = ['calzone', 'margharita', 'olivio', 'quattro stagioni', 'verdi']
+
+print("Gesorteerd:", pizzas)
+
+pizzas.append('hawaii')
+print("Toevoeging:", pizzas)
+
+pizzas.remove('olivio')
+print("Verwijderd:", pizzas)
+
+print("Eerste 3:", pizzas[:3])
+
+print("Middelste:", [pizzas[len(pizzas) // 2]])
+
+print("Laatste 3:", pizzas[-3:])
+
+#helaas kreeg ik de de code waarin toegevoegd en verwijderd moest worden niet in één lijn code gedaan.
