ef4d6762b07c491d7843adafc1a103bad8fc0627
jornkingmans <151020968+jornkingmans@users.noreply.github.com>
2023-11-26 20:25:19 +0100

    Update opdr_4.py

--- opdrachten/060_loops/opdr_5/opdr_4.py
+++ opdrachten/060_loops/opdr_5/opdr_4.py
@@ -1,9 +1,10 @@
 # Opdracht 3 input functie
-# Naam student:
+# Naam student: Jorn Kingmans
 # Groep:
 
 # Hier komt je code...
 
 # Hier start de for-loop
 
-my_list = []
+my_list = [x * 3 for x in range(1, 21)]
+print (my_list[:6])
