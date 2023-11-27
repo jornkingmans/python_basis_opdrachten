2ec83a19fa7c10f306632e1fea354ee8ad911963
jornkingmans <151020968+jornkingmans@users.noreply.github.com>
2023-11-27 19:42:16 +0100

    Update opdr_1.py

--- opdrachten/070_condities/opdr_1/opdr_1.py
+++ opdrachten/070_condities/opdr_1/opdr_1.py
@@ -1,12 +1,17 @@
 # Opdracht 1 condities
-# Naam student:
+# Naam student: Jorn Kingmans
 # Groep:
 
 # Hier komt je code, maak gebruik van de input functie om de lengte van de rechthoekzijden van de driehoek op te vragen.
 
 # Hier start de for-loop....
 
-my_list = []
+nummers = []
 
 #for loop
+for nummer in range(1, 11):
 #if statement
+    if nummer > 4:  
+        nummers.append(nummer)
+
+print(nummers)
