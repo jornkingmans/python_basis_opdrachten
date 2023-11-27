7b59832e10a57b4a8cd2b978ceb63684a32d212c
jornkingmans <151020968+jornkingmans@users.noreply.github.com>
2023-11-27 20:08:09 +0100

    Update opdr_4.py

--- opdrachten/070_condities/opdr_4/opdr_4.py
+++ opdrachten/070_condities/opdr_4/opdr_4.py
@@ -1,12 +1,21 @@
 # Opdracht 4 condities
-# Naam student:
+# Naam student: Jorn Kingmans
 # Groep:
 
 
 
 toppings = [("olijven", 4.50), ("kaas", 3.50), ("salami", 3.00), ("pepperoni", 2.00) , ("ansjovis", 2.50)]
-beschikbare_toppings = ...
-
+beschikbare_toppings = [topping[0] for topping in toppings]
 keuze = input(f"Maak een keuze uit onze toppings: {beschikbare_toppings} \n")
 
 # Hier de rest van jouw code...
+
+gevonden = False
+for topping in toppings:
+    if topping[0] == keuze:
+        print(f"U heeft {keuze} besteld. Dat kost {topping[1]}")
+        gevonden = True
+        break
+
+if not gevonden:
+    print("Uw keuze zit niet in osn assortiment")
