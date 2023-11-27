c611da08bba155aa0facd4e77dc09cf03cca335f
jornkingmans <151020968+jornkingmans@users.noreply.github.com>
2023-11-27 20:55:01 +0100

    Update opdr_2.py

--- opdrachten/080_tekstbestanden/opdr_2/opdr_2.py
+++ opdrachten/080_tekstbestanden/opdr_2/opdr_2.py
@@ -1,5 +1,5 @@
 # Opdracht 2 tekstbestanden
-# Naam student:
+# Naam student: Jorn Kingmans
 # Groep:
 
 import random
@@ -8,3 +8,18 @@ geheim_getal = random.randint(1, 100)
 
 # De rest moet jij doen.....
 
+aantal_pogingen = 0
+geraden = False
+
+while not geraden:
+    gok = int(input("Raad het getal: "))
+    aantal_pogingen += 1
+
+    if gok < geheim_getal:
+        print("hoger")
+    elif gok > geheim_getal:
+        print("lager")
+    else:
+        print(f"Je hebt het getal geraden, het is {geheim_getal}")
+        print(f"Je hebt het getal in {aantal_pogingen} keer geraden")
+        geraden = True
