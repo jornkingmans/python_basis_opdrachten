f4fd618587bfa60799e5cc364aca95959eebb6f8
jornkingmans <151020968+jornkingmans@users.noreply.github.com>
2023-11-27 20:40:55 +0100

    Update opdr_1.py

--- opdrachten/080_tekstbestanden/opdr_1/opdr_1.py
+++ opdrachten/080_tekstbestanden/opdr_1/opdr_1.py
@@ -1,5 +1,23 @@
 # Opdracht 1 while-loops
-# Naam student:
+# Naam student: Jorn Kingmans
 # Groep:
 
 # Jouw code komt hier
+
+vragen = [
+    "Wat vind je van de huidige regering?",
+    "Wat vind je van de Python-lessen tot nu toe?",
+    "Wat vind jij de mooiste stad van Nederland?"
+]
+
+antwoorden = []
+for vraag in vragen:
+    antwoord = input(vraag + "\n")
+    antwoorden.append(antwoord)
+
+    bestandspad = r"C:\temp\enquete_resultaten.txt"
+    with open(bestandspad, "w") as bestand:
+        for vraag, antwoord in zip(vragen, antwoorden):
+            bestand.write(vraag + "\n" + antwoord + "\n\n")
+
+print("De resultaten zijn opgeslagen in 'enquete_resultaten.txt'.")
