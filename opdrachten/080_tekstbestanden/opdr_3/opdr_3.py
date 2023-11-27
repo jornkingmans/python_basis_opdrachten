74a4729ef6d711d8b91173d27be3b56c3a87ac45
jornkingmans <151020968+jornkingmans@users.noreply.github.com>
2023-11-27 21:08:05 +0100

    Update opdr_3.py

--- opdrachten/080_tekstbestanden/opdr_3/opdr_3.py
+++ opdrachten/080_tekstbestanden/opdr_3/opdr_3.py
@@ -1,6 +1,21 @@
 # Opdracht 3 Tekst opslaan
-# Naam student:
+# Naam student: Jorn Kingmans
 # Groep:
 
+def encrypt_tekst(tekst, verschuiving):
+    resultaat = ""
+    for teken in tekst:
+        if teken.isalpha():
+            code = ord(teken.lower()) + verschuiving
+            if code > ord('z'):
+                code -= 26
+            resultaat += chr(code)
+        else:
+            resultaat += teken
+    return resultaat
 
+invoer_tekst = input("Geef de tekst die je wilt encrypten:\n")
 
+encrypted_tekst = encrypt_tekst(invoer_tekst, 5)
+
+print("Geëncrypteerde tekst:", encrypted_tekst)
