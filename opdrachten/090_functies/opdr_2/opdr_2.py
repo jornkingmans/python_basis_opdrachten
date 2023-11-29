184f5c32797354e7969af15e97d9c0937b11957f
jornkingmans <151020968+jornkingmans@users.noreply.github.com>
2023-11-29 20:53:51 +0100

    Update opdr_2.py

--- opdrachten/090_functies/opdr_2/opdr_2.py
+++ opdrachten/090_functies/opdr_2/opdr_2.py
@@ -1,20 +1,38 @@
 # Opdracht 1 functies
-# Naam student:
+# Naam student: Jorn Kingmans
 # Groep:
 
 
 def kilometers_naar_miles(km):
-    # je code komt hier
-    # het woordje pass hieronder kun je weghalen
-    pass
+    """
+    Zet kilometers om naar miles.
+    
+    Args:
+    kilometers: Het aantal kilometers.
+
+    Returns:
+    Het aantal miles.
+    """
+    miles = kilometers / 1.609344
+    return miles
 
 def miles_naar_kilometers(miles):
-    # je code komt hier
-    # het woordje pass hieronder kun je weghalen
-    pass
+    """
+    Zet miles om naar kilometers.
+    
+    Args:
+    miles: Het aantal miles.
+
+    Returns:
+    Het aantal kilometers.
+    """
+    kilometers = miles * 1.609344
+    return kilometers
 
 kilometers = 1223
 miles = 867
+omgezette_miles = kilometers_naar_miles(kilometers)
+omgezette_km = miles_naar_kilometers(miles)
 
-print(kilometers_naar_miles(kilometers))
-print(miles_naar_kilometers(miles))
+print(f"{kilometers} kilometers = {omgezette_miles} miles")
+print(f"{miles} miles = {omgezette_km} kilometers")
