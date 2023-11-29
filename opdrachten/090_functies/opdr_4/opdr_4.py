0444aa87a0a222255c5ad0dedf9704aeef2b8585
jornkingmans <151020968+jornkingmans@users.noreply.github.com>
2023-11-29 21:14:39 +0100

    Update opdr_4.py

--- opdrachten/090_functies/opdr_4/opdr_4.py
+++ opdrachten/090_functies/opdr_4/opdr_4.py
@@ -1,13 +1,20 @@
 # Opdracht 1 functies
-# Naam student:
+# Naam student: Jorn Kingmans
 # Groep:
 
 
 def volledige_naam(lijst_met_namen):
-    # hier komt jouw code
-    # Het woordje pass mag je weghalen
-    pass
+    for naam in lijst_met_namen:
+        voornaam = naam["voornaam"]
+        tussenvoegsel = naam["tussenvoegsel"]
+        achternaam = naam["achternaam"]
 
+        if tussenvoegsel:
+            volledige_naam = f"{voornaam} {tussenvoegsel} {achternaam}"
+        else:
+            volledige_naam = f"{voornaam} {achternaam}"
+
+        print(volledige_naam)
 
 namen = [
     {"voornaam": "Willem", "tussenvoegsel": "van", "achternaam": "Dijk"},
@@ -17,3 +24,4 @@ namen = [
 ]
 
 volledige_naam(namen)
+
