0c173baa9d484e75afac6941e9909e6063cd8c5e
jornkingmans <151020968+jornkingmans@users.noreply.github.com>
2023-11-29 21:03:35 +0100

    Update opdr_3.py

--- opdrachten/090_functies/opdr_3/opdr_3.py
+++ opdrachten/090_functies/opdr_3/opdr_3.py
@@ -1,20 +1,36 @@
 # Opdracht 1 functies
-# Naam student:
+# Naam student: Jorn Kingmans
 # Groep:
 
 
+import math
+
 def kubus_vol(m):
-    # je code komt hier
-    # het woordje pass hieronder kun je weghalen
-    pass
+    """
+    Bereken het volume van een kubus.
+
+    Args:
+    m: De lengte van de zijden van de kubus.
+
+    Returns:
+    Het volume van de kubus.
+    """
+    return m ** 3
 
 def bol_vol(r):
-    # je code komt hier
-    # het woordje pass hieronder kun je weghalen
-    pass
+    """
+    Bereken het volume van een bol.
 
-zijde = 5
-radius = 4
+    Args:
+    r: De straal van de bol.
 
-print(kubus_vol(5))
-print(bol_vol(4))
+    Returns:
+    Het volume van de bol.
+    """
+    return (4/3) * math.pi * r ** 3
+
+kubus_volume = kubus_vol(5)
+bol_volume = bol_vol(4)
+
+print(f"De inhoud van deze kubus is: {kubus_volume}")
+print(f"De inhoud van deze bol is: {bol_volume}")
